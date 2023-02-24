4b7689496d88cf5a98c1b362c74ff3559efbb6d3
rjo6615 <robertjobernier@gmail.com>
2023-02-24 13:53:04 -0500

    added starter files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
