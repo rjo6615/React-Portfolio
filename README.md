42c46fdff7a4a4a71825471102057a99264308b6
rjo6615 <robertjobernier@gmail.com>
2023-02-28 13:26:01 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # React-Portfolio
+
+## Description
+
+While learning web development it is important to put together a portfolio so
+I put together this project to display my work and information to possible employers.
+This project allowed me to display my skills in building a web page while providing useful
+information to click through about my work and myself. It was also a good learning process
+where I could utalize the skills ive learned thus far to put together a functional website.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+To run this project, the user needs to download the repository, run npm install, then npm start. It can also be accessed by utalizing github live pages from this link. https://rjo-portfolio.herokuapp.com/
+
+## Usage
+
+To use this project, start with the navigation bar on the top of the page. Select an option and the site
+will move you to the selected section on the page. The resume option will direct you to a pdf of my resume.
+Under the Portfolio section, the six projects listed are all clickable and will direct you to the selected project.
+In the contact section, there is a form that can be filled out to send me a message.
+
+Example images are listed below:
+
+![Nav bar](./images/about.jpg)
+![Work section](./images/portfolio.jpg)
+![Email example](./images/contact.jpg)
+
+## Credits
+
+John Ifert-Miller
+https://github.com/johnboy514
+
+## License
+
+N/A
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Route, Routes, Link } from "react-router-dom";
 import {  BrowserRouter as Router,  Route,  Routes,  Navigate} from "react-router-dom";
 import About from "./components/About/About";
 import Portfolio from "./components/Portfolio/Portfolio";
@@ -10,87 +9,7 @@ import Footer from "./components/Footer/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// class App extends React.Component {
-//   render() {
-//       return (
-//         <div className="App">
-//           <div>
-//             <nav>
-//               <ul id="navigation">
-//                 <li>
-//                   <Link to="/">Home</Link>
-//                 </li>
-//                 <li>
-//                 <Link to="/about">About</Link>
-//                 </li>
-//                 <li>
-//                 <Link to="/contact">Contact</Link>
-//                 </li>
-//               </ul>
-//             </nav>
-//           </div>
-//           <Routes>
-//             <Route exact path="/">
-//               <About />
-//             </Route>
-//             <Route path="/portfolio">
-//               <Portfolio />
-//             </Route>
-//             <Route path="/contact">
-//               <Contact />
-//             </Route>
-//             <Route path="/resume">
-//               <Resume />
-//             </Route>
-//           </Routes>
-//           </div>
-//             );
-//   }
-// }
-
-// class App extends React.Component {
-//   render() {
-//       return (
-            
-      
-      
-//         <div className="App">
-//           <Navbar />
-//             <Routes>
-//             <Route exact path="/">
-//               <About />
-//             </Route>
-//             <Route path="/portfolio">
-//               <Portfolio />
-//             </Route>
-//             <Route path="/contact">
-//               <Contact />
-//             </Route>
-//             <Route path="/resume">
-//               <Resume />
-//             </Route>
-//           </Routes>
-//           <Footer />
-//           </div>
-//             );
-//   }
-// }
-
 function App() {
-  // let component
-  // switch (window.location.pathname) {
-  //   case "/portfolio":
-  //     component = <Portfolio />
-  //     break  
-  //   case "/contact":
-  //     component = <Contact />
-  //     break  
-  //   case "/resume":
-  //     component = <Resume />
-  //     break; 
-  //   default:
-  //   component = <About />
-  // }
   return (
 
     <Router>
@@ -104,11 +23,6 @@ function App() {
         </Routes>
         <Footer />
     </Router>
-    // <div>
-    //   <Navbar />
-    //   {component}
-    //   <Footer />
-    // </div>
   );
 }
 
