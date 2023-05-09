ac3ef7bed2e82beb9b777cd287f2907cf43753cd
rjo6615 <robertjobernier@gmail.com>
2023-05-09 12:12:41 -0400

    added tech used and github link to portfolio page

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,39 +8,43 @@ function Portfolio() {
         {
             name: "Fit Quest",
             description: "Workout and routine creator",
+            tech: "React, MongoDB, Express.js, Node.js, Bootstrap",
+            repo: "https://github.com/rjo6615/Fit-Quest",
             link: "https://fitquest.herokuapp.com/",
             image: "./images/fitquest.jpg" 
         },
         {
             name: "G.R.I.M.S",
             description: "Inventory management system",
+            tech: "Handlebars, mySQL, Express.js, Node.js, Bootstrap, Cloudinary, EmailJS",
+            repo: "https://github.com/Eaponte24/GRIMS",
             link: "https://grims-inventory.herokuapp.com/",
             image: "./images/grims.jpg" 
         },
         {
             name: "Rainy Day Movies",
             description: "Movie info lookup",
+            tech: "HTML5, CSS3, YoutubeAPI, Javascript",
+            repo: "https://github.com/EmilyBernard/rainy-day-movies",
             link: "//emilybernard.github.io/rainy-day-movies/",
             image: "./images/rainy-day-movies.jpg" 
         },
         {
             name: "Weather Dashboard",
             description: "5 day weather forcast look up",
+            tech: "Bootstrap, WeatherAPI, Javascript",
+            repo: "https://github.com/rjo6615/Weather-Dashboard",
             link: "//rjo6615.github.io/Weather-Dashboard/",
             image: "./images/weather-dashboard.jpg" 
         },
         {
             name: "Code Quiz",
             description: "A timed coding quiz",
+            tech: "Javascript, Bootstrap",
+            repo: "https://github.com/rjo6615/Code-Quiz",
             link: "//rjo6615.github.io/Code-Quiz/",
             image: "./images/code-quiz.jpg" 
         },
-        {
-            name: "Work Day Scheduler",
-            description: "Work scheduling calendar",
-            link: "https://rjo6615.github.io/Work-Day-Scheduler/",
-            image: "./images/work-day-scheduler.jpg"  
-       },
       ];
     return (
       <center className="pb-4">
@@ -48,14 +52,22 @@ function Portfolio() {
       <Row xs={1} md={2} lg={3} xl={4} className="g-4 mx-4">
         {projects.map((el) => (         
           <Col>          
-            <Card style={{ maxWidth: 300, minHeight: 450, maxHeight: 350, backgroundColor: "lightGray"}}>
+            <Card style={{ minWidth: 250, maxWidth: 300, minHeight: 620, maxHeight: 620, backgroundColor: "lightGray"}}>
               <Card.Img className="mx-auto" style={{ marginBottom: '20px', marginTop: '15px', maxWidth: '16rem', borderRadius: '25px'}} variant="top" src={el.image} />              
               <Card.Body>
                 <Card.Title>{el.name}</Card.Title>
                 <Card.Text>
                 {el.description}
+                <br/><br/>
+                <h6>Tech Used:</h6>
+                {el.tech}
                 </Card.Text>
               </Card.Body>
+              <a href={el.repo} >
+              <button type="button" class="btn btn-sm mb-3 px-5"
+							style={{color: "white", backgroundColor: "darkblue", borderRadius: 25}}>
+                <strong>View Github Repo</strong></button>
+                </a>
               <a href={el.link} >
               <button type="button" class="btn btn-sm mb-3 px-5"
 							style={{color: "white", backgroundColor: "darkblue", borderRadius: 25}}>
